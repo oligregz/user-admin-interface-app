15a9560b57b124fae0e4e463e21df020ea55f149
oligregz <gregorysak90@gmail.com>
2023-06-21 19:00:38 -0300

    feat: vuex scope implemented

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    user: null,
   },
   getters: {
   },
   mutations: {
+    SET_USER (state, user) {
+      state.user = user
+    },
+
+    CLEAR_USER (state) {
+      state.user = null
+    }
   },
   actions: {
+    async register({commit}, details) {
+      //
+    },
+    
+    async login({commit}, details) {
+      //
+    },
+
+    async logout({commit}) {
+      //
+    }
   },
+
   modules: {
   }
-})
+});
